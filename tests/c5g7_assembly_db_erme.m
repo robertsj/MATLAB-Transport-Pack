2f01fe5a648eeb4fedcc3df6534d67b3ffd127a9
Jeremy Roberts <robertsj@mit.edu>
2012-03-22 07:54:32 -0400

    c5g7 erme test

--- tests/c5g7_assembly_db_erme.m
+++ tests/c5g7_assembly_db_erme.m
@@ -1,143 +1,62 @@
-%> @file  c5g7_pin_db_erme.m
-%> @brief Generate response database for C5G7 assemblies cells.
-%
+%> @file  test_erme.m
+%> @brief Demo of ERME and solvers.
 
 clear
-tic
+
 % Include the MTP source directory
 path(path, '../')
-Mesh();
-% ==============================================================================
-% Create a response DB
-% ==============================================================================
 
-inputrf = Input();
-put(inputrf, 'number_groups',         7);
-put(inputrf, 'dimension',             2);
-put(inputrf, 'inner_tolerance',       1e-12);
-put(inputrf, 'inner_max_iters',       100);
-put(inputrf, 'outer_tolerance',       1e-9);
-put(inputrf, 'outer_max_iters',       10);
-put(inputrf, 'inner_print_out',       1);
-put(inputrf, 'rf_print_out',          1);
-put(inputrf, 'quad_order',            2);
-put(inputrf, 'rf_max_order_space',    0);
-put(inputrf, 'rf_max_order_azimuth',  0);
-put(inputrf, 'rf_max_order_polar',    0);
-% Different keff vector for the moderator (i.e. 1 value)
-keff_vectors    = cell(3, 1);
-keff_vectors{1} = [0.9; 1.0; 1.1; 1.2; 1.3];
-keff_vectors{2} = [0.9; 1.0; 1.1; 1.2; 1.3];
-keff_vectors{3} = [1.0];
-put(inputrf, 'rf_keff_vector',        keff_vectors);
-put(inputrf, 'rf_number_nodes',       3);
-put(inputrf, 'rf_db_name',            'c5g7_assemblies.h5');
+tic
 
+input = Input();
+
+so = [0 1 2 4];
+ao = [0 2 4];
+po = [0 2];
+
+for s = 4
+    for a = 3
+        for p = 2
+
+            
+            disp([' so = ',num2str(s),' ao = ',num2str(a),' po = ',num2str(p)])
 % ==============================================================================
-% MATERIALS
+% Server
 % ==============================================================================
-mat = C5G7_materials(0);
+put(input, 'rf_db_name', 'c5g7_assemblies.h5');
+put(input, 'rf_interp_method', 'spline');
+put(input, 'rf_order_space',        so(s)); % Eventually, these will be the
+put(input, 'rf_order_azimuth',      ao(a)); % orders extracted from a DB
+put(input, 'rf_order_polar',        po(p)); 
+server = ResponseDB(input);
+read_response(server);
 
 % ==============================================================================
-% PINS
+% ERME setup
 % ==============================================================================
+put(input, 'inner_tolerance',       1e-10);
+put(input, 'inner_max_iters',       100);
+put(input, 'outer_tolerance',       1e-9);
+put(input, 'outer_max_iters',       20);
+put(input, 'number_groups',         7);
+put(input, 'dimension',             2);
+put(input, 'bc_left',               'reflect');
+put(input, 'bc_right',              'vacuum');
+put(input, 'bc_top',                'reflect');
+put(input, 'bc_bottom',             'vacuum');
 
-% Shared pin properties
-pitch = 1.26; radii = 0.54; number = 3; %52;
-% Pin 1 - UO2 
-matid = [1 7];  pin1 = PinCell(pitch, radii, matid); meshify(pin1, number);
-% Pin 2 - 4.3% MOX
-matid = [2 7];  pin2 = PinCell(pitch, radii, matid); meshify(pin2, number);
-% Pin 3 - 7.0% MOX
-matid = [3 7];  pin3 = PinCell(pitch, radii, matid); meshify(pin3, number);
-% Pin 4 - 8.7% MOX
-matid = [4 7];  pin4 = PinCell(pitch, radii, matid); meshify(pin4, number);
-% Pin 5 - Guide Tube
-matid = [5 7];  pin5 = PinCell(pitch, radii, matid); meshify(pin5, number);
-% Pin 6 - Fission Chamber
-matid = [6 7];  pin6 = PinCell(pitch, radii, matid); meshify(pin6, number);
-% Pin 7 - Moderator
-matid = [7  ];  pin7 = PinCell(pitch, [   ], matid); meshify(pin7, number);
+map = [ 1 2 3
+        2 1 3
+        3 3 3 ];
+problem = ERME(input, server, map);
+solver  = ERME_Picard(input, problem); 
+solve(solver);   
 
+toc
 
-% ==============================================================================
-% ASSEMBLIES
-% ==============================================================================
-
-G = 5; F = 6;
-
-% Assembly 1 - UO2
-pin_map1 = [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-            1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-            1 1 1 1 1 G 1 1 G 1 1 G 1 1 1 1 1 
-            1 1 1 G 1 1 1 1 1 1 1 1 1 G 1 1 1 
-            1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-            1 1 G 1 1 G 1 1 G 1 1 G 1 1 G 1 1 
-            1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-            1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-            1 1 G 1 1 G 1 1 F 1 1 G 1 1 G 1 1 
-            1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-            1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-            1 1 G 1 1 G 1 1 G 1 1 G 1 1 G 1 1 
-            1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-            1 1 1 G 1 1 1 1 1 1 1 1 1 G 1 1 1 
-            1 1 1 1 1 G 1 1 G 1 1 G 1 1 1 1 1 
-            1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-            1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 ];     
-assem1 = Assembly({pin1, pin2, pin3, pin4, pin5, pin6, pin7}, pin_map1); 
-meshify(assem1);
-% Assembly 2 - MOX
-pin_map2 = [2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
-            2 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 2
-            2 3 3 3 3 G 3 3 G 3 3 G 3 3 3 3 2
-            2 3 3 G 3 4 4 4 4 4 4 4 3 G 3 3 2
-            2 3 3 3 4 4 4 4 4 4 4 4 4 3 3 3 2
-            2 3 G 4 4 G 4 4 G 4 4 G 4 4 G 3 2
-            2 3 3 4 4 4 4 4 4 4 4 4 4 4 3 3 2
-            2 3 3 4 4 4 4 4 4 4 4 4 4 4 3 3 2
-            2 3 G 4 4 G 4 4 F 4 4 G 4 4 G 3 2
-            2 3 3 4 4 4 4 4 4 4 4 4 4 4 3 3 2
-            2 3 3 4 4 4 4 4 4 4 4 4 4 4 3 3 2
-            2 3 G 4 4 G 4 4 G 4 4 G 4 4 G 3 2
-            2 3 3 3 4 4 4 4 4 4 4 4 4 3 3 3 2
-            2 3 3 G 3 4 4 4 4 4 4 4 3 G 3 3 2
-            2 3 3 3 3 G 3 3 G 3 3 G 3 3 3 3 2
-            2 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 2
-            2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2];
-
-assem2 = Assembly({pin1, pin2, pin3, pin4, pin5, pin6, pin7}, pin_map2); 
-meshify(assem2);
-% Assembly 3 - Moderator
-pin_map3 = [7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
-            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
-            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
-            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
-            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
-            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
-            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
-            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
-            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
-            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
-            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
-            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
-            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
-            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
-            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
-            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
-            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7];
-assem3 = Assembly({pin1, pin2, pin3, pin4, pin5, pin6, pin7}, pin_map3); 
-meshify(assem3);
-
-% ==============================================================================
-% RESPONSE FUNCTION DRIVER
-% ==============================================================================
-mesh_array = {assem1, assem2, assem3};
-
-
-driver = ResponseDriver(inputrf, mat, mesh_array);
-run(driver);
-
-
+        end
+    end
+end
 
 
 
